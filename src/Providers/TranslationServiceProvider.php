8055d3bb40d881f0b99bbff1aee08e61d9dae799
Hamid Ghorashi <h.ghorashi@gmail.com>
2024-06-28 17:25:28 +0400

    create service provider

---
+++ src/Providers/TranslationServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Jetcode\Laravel\Translation\Providers;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+class TranslationServiceProvider extends LaravelServiceProvider
+{
+    public function register() {}
+
+    public function boot()
+    {
+        $this->offerPublishing();
+    }
+
+    protected function offerPublishing(): void
+    {
+        if (!$this->app->runningInConsole()) {
+            // Refuse publishing configuration values in non-console environments
+            return;
+        }
+
+        if (!function_exists('config_path') || !function_exists('database_path')) {
+            // Refuse publishing configuration values in Lumen
+            return;
+        }
+
+        $this->publishes([
+            __DIR__ . '/../config/translations.php' => config_path('translations.php'),
+        ], 'translation-config');
+
+        $this->publishes([
+            __DIR__ . '/../Migrations/2024_06_28_000000_create_translations_table.php' => database_path('migrations/' . date('Y_m_d_His') . '_create_translations_table.php'),
+        ], 'translation-migrations');
+    }
+}
