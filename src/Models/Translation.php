c98b5c6ad4a4fddec540ca44f5db8b49047fb11f
Hamid Ghorashi <h.ghorashi@gmail.com>
2024-06-28 17:42:36 +0400

    create Translation model

---
+++ src/Models/Translation.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Jetcode\Laravel\Translation\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+/**
+ * Class Translation.
+ *
+ * @property string translatable_type
+ * @property int translatable_id
+ * @property string lang
+ * @property string title
+ * @property string text
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ * @property Carbon deleted_at
+ */
+class Translation extends Model
+{
+    /**
+     * @var string[]
+     */
+    protected $guarded = ['id'];
+
+    protected $fillable = [
+        'translatable_id',
+        'translatable_type',
+        'lang',
+        'title',
+        'text',
+    ];
+
+    public function translatable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    /**
+     * @return string
+     */
+    public function getTable()
+    {
+        return config('translations.table');
+    }
+}
