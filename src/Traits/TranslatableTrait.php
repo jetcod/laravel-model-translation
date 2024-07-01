105c5be3e23401a0b53031686053aaf8e00a2571
Hamid Ghorashi <h.ghorashi@gmail.com>
2024-07-01 13:18:49 +0400

    only translate defined attributes

--- src/Traits/TranslatableTrait.php
+++ src/Traits/TranslatableTrait.php
@@ -11,16 +11,6 @@ trait TranslatableTrait
 {
     private $cachedModel;
 
-    public function translation(): MorphOne
-    {
-        return $this->morphOne(Translation::class, 'translatable');
-    }
-
-    public function translations(): MorphMany
-    {
-        return $this->morphMany(Translation::class, 'translatable');
-    }
-
     public function __get($key)
     {
         if ($this->isRelation($key)) {
@@ -36,15 +26,52 @@ trait TranslatableTrait
         return parent::__get($key);
     }
 
-    private function getTranslation($key)
+    public function translation(): MorphOne
     {
-        $model = $this->getCachedModel();
+        return $this->morphOne(Translation::class, 'translatable');
+    }
 
-        if ($model instanceof Model) {
-            return $key == $model->key ? $model->value : null;
+    public function translations(): MorphMany
+    {
+        return $this->morphMany(Translation::class, 'translatable');
+    }
+
+    /**
+     * Returns translatable attribute(s).
+     *
+     * @return null|array|string
+     */
+    protected function getTranslatableAttributes()
+    {
+        if (defined('static::TRANSLATABLE_ATTRIBUTES')) {
+            return static::TRANSLATABLE_ATTRIBUTES;
         }
 
         return null;
+    }
+
+    private function getTranslation(string $key): ?string
+    {
+        if ($this->isTranslatableAttribute($key)) {
+            $model = $this->getCachedModel();
+
+            if ($model instanceof Model) {
+                return $key == $model->key ? $model->value : null;
+            }
+        }
+
+        return null;
+    }
+
+    private function isTranslatableAttribute(string $key): bool
+    {
+        $translatables = $this->getTranslatableAttributes();
+
+        if (is_null($translatables)) {
+            return true;
+        }
+
+        return is_array($translatables) ? in_array($key, $translatables) : $key == $translatables;
     }
 
     private function getCachedModel()
